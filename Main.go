16dcb754bdb0caf7cf6f7f3f36e76a8faaf428ac
Matthew Sedlacek <sedlacek1991@gmail.com>
2021-01-06 22:02:28 -0800

    adds more slice examples

--- Main.go
+++ Main.go
@@ -3,12 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	//slice
-	a := []int{1, 2, 3}
-	fmt.Println(a)
+	a := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	b := a[:]   // slice of all elements
+	c := a[3:]  // slice from 4th element to end
+	d := a[:6]  // slice first 6 elements
+	e := a[3:6] // slice the 4th, 5th, and 6th elements
 
-	// Capacity and length can be different. A Slice can only be a portion of the actual array
-	fmt.Printf("Length: %v\n", len(a))
-	fmt.Printf("Capacity: %v\n", cap(a))
+	// changes to one of the slices changes all other slices
+	b[5] = 42
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+	fmt.Println(e)
 
 }
