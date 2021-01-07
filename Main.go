04c7ba2cca8a4fba49824f600de37972d590a775
Matthew Sedlacek <sedlacek1991@gmail.com>
2021-01-06 23:21:16 -0800

    show concatenate and stack operations in slices

--- Main.go
+++ Main.go
@@ -4,14 +4,34 @@ import "fmt"
 
 func main() {
 	a := []int{}
-	fmt.Println(a)
-	fmt.Printf("Length: %v\n", len(a))
-	fmt.Printf("Capacity: %v\n", cap(a))
+	// fmt.Println(a)
+	// fmt.Printf("Length: %v\n", len(a))
+	// fmt.Printf("Capacity: %v\n", cap(a))
 
 	// Adding an element to a slice
 	a = append(a, 1)
-	fmt.Println(a)
-	fmt.Printf("Length: %v\n", len(a))
-	fmt.Printf("Capacity: %v\n", cap(a))
+	// fmt.Println(a)
+	// fmt.Printf("Length: %v\n", len(a))
+	// fmt.Printf("Capacity: %v\n", cap(a))
+
+	// Concatenating Slices
+	a = append(a, []int{2, 3, 4, 5}...)
+	// fmt.Println(a)
+	// fmt.Printf("Length: %v\n", len(a))
+	// fmt.Printf("Capacity: %v\n", cap(a))
+
+	// Stack Operations
+
+	// remove element from the beginning
+	// b := a[1:]
+	// fmt.Println(b)
+
+	// remove element from the end of an array
+	// b := a[:len(a)-1]
+	// fmt.Println(b)
+
+	// remove element from the middle of an array
+	b := append(a[:2], a[3:]...)
+	fmt.Println(b)
 
 }
