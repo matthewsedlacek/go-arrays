ec1dcf0eee3353a39fe800ac7691538b051741f2
Matthew Sedlacek <sedlacek1991@gmail.com>
2021-01-27 11:02:04 -0800

    add array example with data inputs provided

--- Main.go
+++ Main.go
@@ -3,35 +3,41 @@ package main
 import "fmt"
 
 func main() {
-	a := []int{}
-	// fmt.Println(a)
-	// fmt.Printf("Length: %v\n", len(a))
-	// fmt.Printf("Capacity: %v\n", cap(a))
+	fastestServes := []float64{163.4, 160.0, 157.2}
+	fmt.Println(fastestServes)
+	// newFastestServes := [3]float64{}
+	// fmt.Println(newFastestServes)
+	// newFastestServes[0] = 168.2
+	// newFastestServes[1] = 167.5
+	// newFastestServes[2] = 166.1
+	// fmt.Println(newFastestServes)
+	// fmt.Printf("Length: %v\n", len(fastestServes))
+	// fmt.Printf("Capacity: %v\n", cap(fastestServes))
 
 	// Adding an element to a slice
-	a = append(a, 1)
-	// fmt.Println(a)
-	// fmt.Printf("Length: %v\n", len(a))
-	// fmt.Printf("Capacity: %v\n", cap(a))
+	// a = append(a, 1)
+	// // fmt.Println(a)
+	// // fmt.Printf("Length: %v\n", len(a))
+	// // fmt.Printf("Capacity: %v\n", cap(a))
 
-	// Concatenating Slices
-	a = append(a, []int{2, 3, 4, 5}...)
-	// fmt.Println(a)
-	// fmt.Printf("Length: %v\n", len(a))
-	// fmt.Printf("Capacity: %v\n", cap(a))
+	// // Concatenating Slices
+	// a = append(a, []int{2, 3, 4, 5}...)
+	// // fmt.Println(a)
+	// // fmt.Printf("Length: %v\n", len(a))
+	// // fmt.Printf("Capacity: %v\n", cap(a))
 
-	// Stack Operations
+	// // Stack Operations
 
-	// remove element from the beginning
-	// b := a[1:]
+	// // remove element from the beginning
+	// // b := a[1:]
+	// // fmt.Println(b)
+
+	// // remove element from the end of an array
+	// // b := a[:len(a)-1]
+	// // fmt.Println(b)
+
+	// // remove element from the middle of an array
+	// b := append(a[:2], a[3:]...)
 	// fmt.Println(b)
-
-	// remove element from the end of an array
-	// b := a[:len(a)-1]
-	// fmt.Println(b)
-
-	// remove element from the middle of an array
-	b := append(a[:2], a[3:]...)
-	fmt.Println(b)
 
 }
